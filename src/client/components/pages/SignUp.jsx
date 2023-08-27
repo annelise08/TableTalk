6b56293fd43b32fede6b1f57b17e2922ec3e620d
Anne-lise Emig <anneliseemig@gmail.com>
2023-08-27 17:20:07 -0500

    basic sign up page set up

--- src/client/components/pages/SignUp.jsx
+++ src/client/components/pages/SignUp.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const SignUp = () => {
+  const [fullname, setFullname] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState('');
+
+  const handleFullname = (e) => {
+    setFullname(e.target.value)
+  }
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
   return (
     <>
-      <h1>This is the signup page</h1>
+      <h1>Welcome to TableTalk!</h1>
+      <form action="">
+        <input type="text" id="fullname" placeholder="Full name" onChange={handleFullname}/>
+        <input type="text" id="username" placeholder="Username" onChange={handleUsername}/>
+        <input type="password" placeholder="Password" onChange={handlePassword}/>
+        <button>Sign up</button>
+      </form>
     </>
   );
 };
